85bca3436c70020a648bcb666c877c313b9046e4
f <franklinrohan2002@gmail.com>
2023-05-04 22:04:39 +0530

    chore:attemp to make a nav bar

---
+++ src/componets/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div>
+      <div>
+        <input></input>
+        <button></button>
+      </div>
+      <div>
+        <h1 className="header1">Nerd Store</h1>
+      </div>
+      <diV>
+        <button>Profile</button>
+      </diV>
+    </div>
+  );
+}
+
+export default NavBar;
