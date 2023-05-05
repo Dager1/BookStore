cfa15d745f07cf852f7f882d1a560de8efa3bd08
f <franklinrohan2002@gmail.com>
2023-05-05 19:11:35 +0530

    chore:added usestate to the input field

--- src/componets/Login.jsx
+++ src/componets/Login.jsx
@@ -3,26 +3,34 @@ import "./Login.css";
 
 function Login() {
   const [email, setEmail] = useState("");
-  const [password, setpassword] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <div className="login">
       <form onClick>
         <div>Login</div>
         <div>
           <label>Email:</label>
-          <input value={email}></input>
+          <input value={email} onChange={handleEmailChange}></input>
         </div>
         <div>
           <label>Password:</label>
-          <input value={password}></input>
+          <input value={password} onChange={handlePasswordChange}></input>
         </div>
         <div>
-          <div>
+          <div className="log">
             <a>Dont have an account?</a>
-          </div>
-          <div>
             <button>Login</button>
           </div>
+          <div></div>
         </div>
       </form>
     </div>
