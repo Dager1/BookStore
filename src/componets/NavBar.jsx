560142527e841981975bce2063b9d57ff2ab6b5d
f <franklinrohan2002@gmail.com>
2023-05-06 21:11:31 +0530

    chore:made a feacth to google book api

--- src/componets/NavBar.jsx
+++ src/componets/NavBar.jsx
@@ -1,26 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./NavBar.css";
 import axios from "axios";
 
 function NavBar() {
-  const feachdata = async () => {
-    const respose = await axios.get();
+  const [name, setName] = useState("");
+  const [results, setResults] = useState([]);
+
+  const handelNameChange = (event) => {
+    setName(event.target.value);
+    console.log(name);
   };
+
+  const feactData = async (event) => {
+    event.preventDefault();
+    const res = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${name}&key=${
+        import.meta.env.VITE_API_KEY
+      }` + `&maxResults=10`
+    );
+    setResults(res.data.items);
+    console.log(res.data.items);
+  };
+
   return (
     <div>
       <div className="navbar">
         <div>
-          <input></input>
-          <button>Serach</button>
+          <input value={name} onChange={handelNameChange} />
+          <button onClick={feactData}>Search</button>
         </div>
         <div>
           <div className="title">Nerd Store</div>
         </div>
-        <diV>
+        <div>
           <button>Profile</button>
-        </diV>
+        </div>
       </div>
-      <div></div>
+      <div>
+        {results.map((result) => {
+          return (
+            <img
+              src={result.volumeInfo.imageLinks.smallThumbnail}
+              alt={result.title}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
